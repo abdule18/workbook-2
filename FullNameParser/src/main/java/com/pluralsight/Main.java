ee3af0e35337aaae1aa474fffb333b376a84aacf
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-17 15:03:38 -0400

    made full name parser

---
+++ FullNameParser/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your full name?: ");
+        String name = scanner.nextLine().trim();
+
+        int firstSpaceIndex = name.indexOf(" ");
+
+        String firstName = name.substring(0, firstSpaceIndex);
+
+        int secondSpaceIndex = name.indexOf(" ", firstSpaceIndex + 1);
+
+        String middleName = "";
+        String lastName = "";
+
+        if (secondSpaceIndex >= 0){
+            middleName = name.substring(firstSpaceIndex + 1, secondSpaceIndex);
+            lastName = name.substring(secondSpaceIndex + 1);
+        } else {
+            middleName = "(None)";
+            lastName = name.substring(firstSpaceIndex + 1);
+        }
+
+        System.out.println();
+        System.out.println("First Name: " + firstName);
+        System.out.println("Middle Name: " + middleName);
+        System.out.println("Last Name: " + lastName);
+    }
+}
