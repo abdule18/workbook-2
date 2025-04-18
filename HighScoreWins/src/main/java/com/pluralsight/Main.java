c5a5a86be0863344d31bbad767df694e864593c5
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-18 09:26:58 -0400

    Updated the variables and added an else if statement

--- HighScoreWins/src/main/java/com/pluralsight/Main.java
+++ HighScoreWins/src/main/java/com/pluralsight/Main.java
@@ -11,34 +11,49 @@ public class Main {
 
         String[] piping = userPrompt.split(Pattern.quote("|"));
 
-        String home = piping[0];
-        String home1 = piping[1];
+        String teamNamesString = piping[0]; //this would be home:visitor
+        String scoresString = piping[1]; //this would be  55:23
 
-        String[] colon = home.split(Pattern.quote(":"));
-        String[] colon1 = home1.split(Pattern.quote(":"));
-        String visit = colon[0];
-        String visit1 = colon[1];
-        int visit2 = Integer.parseInt(colon1[0]);
-        int visit3 = Integer.parseInt(colon1[1]);
+        String[] teamNamesArray = teamNamesString.split(Pattern.quote(":"));
+
+        String[] scoresArray = scoresString.split(Pattern.quote(":"));
+
+        String homeTeamName = teamNamesArray[0];
+        String visitorTeamName = teamNamesArray[1];
+
+        int homeTeamScore = Integer.parseInt(scoresArray[0]);
+        int visitorTeamScore = Integer.parseInt(scoresArray[1]);
 
 
-        if (visit2 > visit3){
-            System.out.println("Winner: " + visit);
-        }  else {
-            System.out.println("Winner: " + visit1);
+        if (homeTeamScore > visitorTeamScore){
+            System.out.println("Winner: " + homeTeamName);
+        }  else if (homeTeamScore == visitorTeamScore){
+            System.out.println("It's a Draw");
+        } else {
+            System.out.println("Winner: " + visitorTeamName);
         }
 
 
 
-//        String input = "111|Hot Chocolate (12-count)|21|4.99";
-//        String[] tokens = input.split(Pattern.quote("|"));
-//        int id = Integer.parseInt(tokens[0]);
-//        String name = tokens[1];
-//        int quantity = Integer.parseInt(tokens[2]);
-//        double price = Double.parseDouble(tokens[3]);
-//        System.out.println(id);
-//        System.out.println(name);
-//        System.out.println(quantity);
-//        System.out.println(price);
+
+
+
+
+        //        String home = piping[0];
+//        String home1 = piping[1];
+//
+//        String[] colon = home.split(Pattern.quote(":"));
+//        String[] colon1 = home1.split(Pattern.quote(":"));
+//        String visit = colon[0];
+//        String visit1 = colon[1];
+//        int visit2 = Integer.parseInt(colon1[0]);
+//        int visit3 = Integer.parseInt(colon1[1]);
+//
+//
+//        if (visit2 > visit3){
+//            System.out.println("Winner: " + visit);
+//        }  else {
+//            System.out.println("Winner: " + visit1);
+//        }
     }
 }
