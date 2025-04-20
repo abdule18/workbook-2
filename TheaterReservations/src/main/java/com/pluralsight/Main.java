18b381ab7f7c12429536f8091137634a7e9ce38f
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-20 10:32:08 -0400

    I make the Theater Reservation

---
+++ TheaterReservations/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,42 @@
+package com.pluralsight;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("What date will you be coming? (MM/dd/yyyy): ");
+        String dateInput = scanner.nextLine();
+
+        System.out.print("How many ticket would you like? ");
+        int tickets = scanner.nextInt();
+
+        int spacePosition = name.indexOf(" ");
+        String firstName = name.substring(0, spacePosition);
+        String lastName = name.substring(spacePosition + 1);
+
+        name = lastName + ", "  + firstName;
+
+
+        String[] date = dateInput.split("\\/");
+
+        String month = date[0];
+        String day = date[1];
+        String year = date[2];
+
+        String finalDate = month + "-" + day + "-" + year;
+
+
+
+        System.out.printf("%d ticket reserved for %s under %s", tickets, finalDate, name);
+
+
+    }
+}
