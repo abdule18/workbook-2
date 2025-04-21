adf0605c2a1bbc91de77d02377dd2a91da317dd8
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-21 13:51:00 -0400

    I made Test Statistic

---
+++ TestStatistics/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {63, 65, 95, 53, 100, 80, 35, 31, 70, 88};
+        int max = nums[0];
+        int min = nums[0];
+        int sum = 0;
+        int average;
+
+        for (int num : nums){
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+
+            sum += num;
+        }
+
+        average = sum/nums.length;
+
+        System.out.println("The Maximum is: " + max);
+        System.out.println("The min is: " + min);
+        System.out.println("The average is: " + average);
+
+    }
+
+//    public static void displayNumbers(int[] nums)
+//    {
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.print(nums[i] + " ");
+//        }
+//        System.out.println();
+//    }
+}
